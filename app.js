67b1735658de77822cacb9d956874af3ca219d5c
SanaHameed2 <sana.faiza2@gmail.com>
2025-06-06 17:32:21 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+let P= prompt ("Enter Priiciple amount (P):");
+let R= prompt ("Enter Priiciple amount (R):");
+let T= prompt ("Enter Priiciple amount (T):");
+let SI=(P>0 && R>0 && T>0) ? (P*R*T)/100 :0;
+alert("Simple Interest is: " + SI);
